4d90b470352a49e520fee30b98ef274dc96d69bd
T J Byars <55961586+tjbyars@users.noreply.github.com>
2024-04-21 17:17:32 +0100

    API call setup
    User input reg number and returns entry id

--- api_access.py
+++ api_access.py
@@ -1,13 +1,34 @@
 import boto3
 
-# Initialize Boto3 S3 client
-s3 = boto3.client('s3')
+# Initialize Boto3 DynamoDB client
+dynamodb = boto3.client('dynamodb')
 
-# Specify bucket name and file key
-bucket_name = 'ev-telematics'
-file_key = 'telematics_data.csv'
+# Specify the table name
+table_name = 'EV-Telematics'
 
-# Download the dataset
-s3.download_file(bucket_name, file_key, 'dataset_download.csv')
+# Prompt the user to enter the ID they want to query
+id_value = input("Enter the ID you want to query: ")
 
-# Now you can work with your dataset locally
+# Define the key condition expression for the query
+key_condition_expression = 'id = :idval'
+
+# Define the expression attribute values for the query
+expression_attribute_values = {
+    ':idval': {'S': id_value}
+}
+
+# Define the projection expression to only include the 'reg_number' attribute in the result
+projection_expression = 'reg_number'
+
+# Perform the query
+response = dynamodb.query(
+    TableName=table_name,
+    KeyConditionExpression=key_condition_expression,
+    ExpressionAttributeValues=expression_attribute_values,
+    ProjectionExpression=projection_expression
+)
+
+# Process the query results
+for item in response['Items']:
+    reg_number = item['reg_number']['S']
+    print("Registration Number:", reg_number)
